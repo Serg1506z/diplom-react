f0286cdb58a8ff61021a4f671f0ad36c43ff38db
Serg1506z <81sergejjzabelkin@gmail.com>
2025-04-14 17:21:18 +0300

    add

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -1,8 +1,8 @@
 import Rangeprice from "../RangePrice/RangePrice"
-import {ReactComponent as Cupe} from '../../Assets/Icons/Cupe.svg'
+import { ReactComponent as Cupe } from '../../Assets/Icons/Cupe.svg'
 import style from "./Settings.module.css"
 import { useSelector, useDispatch } from "react-redux"
-import {ReactComponent as CalendarIcon} from "../../Assets/Icons/Calendar.svg"
+import { ReactComponent as CalendarIcon } from "../../Assets/Icons/Calendar.svg"
 import { useEffect, useState } from "react"
 import DateCalendar from "../Calendar/Calendar"
 import { setState } from "../../Redux/Slices/Route"
@@ -12,13 +12,13 @@ import ToggleSetting from "../ToggleSetting/ToggleSetting"
 export default function Settings() {
     const value = useSelector(state => state.route.filterSettings)
     const [isOpen, setIsOpen] = useState([false, false])
-    const setValue = (value) => dispatch(setState(value)) 
+    const setValue = (value) => dispatch(setState(value))
     const dispatch = useDispatch()
-
+    const [openPart, setOpenPart] = useState(false)
     console.log(value);
-    
+
     useEffect(() => {
-        dispatch(getRoutesThunk({...value}))
+        dispatch(getRoutesThunk({ ...value }))
         // eslint-disable-next-line
     }, [value])
 
@@ -27,44 +27,56 @@ export default function Settings() {
         <div className={style.settingDate}>
             <h2 className={style.dateTitle}>Дата поездки</h2>
             <div className={style.input_container}>
-                <input disabled name="dateDeparte" type="text" className={`${style.selectDate} ${style.date}`} value={value.date_start ? new Date(value.date_start).toLocaleDateString('ru-RU', {day: 'numeric', month: 'numeric', year: '2-digit'}) : "ДД/ММ/ГГ"}/>
+                <input disabled name="dateDeparte" type="text" className={`${style.selectDate} ${style.date}`} value={value.date_start ? new Date(value.date_start).toLocaleDateString('ru-RU', { day: 'numeric', month: 'numeric', year: '2-digit' }) : "ДД/ММ/ГГ"} />
                 <CalendarIcon className={style.inputIcon} onClick={() => setIsOpen([!isOpen[0], false])} />
-                {isOpen[0] && <DateCalendar value={value.date_start} setValue={(e) => setValue({...value, date_start: e + ''})} />}
-            </div> 
+                {isOpen[0] && <DateCalendar value={value.date_start} setValue={(e) => setValue({ ...value, date_start: e + '' })} />}
+            </div>
             <h2 className={style.dateTitle}>Дата возвращения</h2>
             <div className={style.input_container}>
-                <input disabled name="dateDeparte" type="text" className={`${style.selectDate} ${style.date}`} value={value.date_end ? new Date(value.date_end).toLocaleDateString('ru-RU', {day: 'numeric', month: 'numeric', year: '2-digit'}) : "ДД/ММ/ГГ"}/>
+                <input disabled name="dateDeparte" type="text" className={`${style.selectDate} ${style.date}`} value={value.date_end ? new Date(value.date_end).toLocaleDateString('ru-RU', { day: 'numeric', month: 'numeric', year: '2-digit' }) : "ДД/ММ/ГГ"} />
                 <CalendarIcon className={style.inputIcon} onClick={() => setIsOpen([false, !isOpen[1]])} />
-                {isOpen[1] &&<DateCalendar value={value.date_end} setValue={(e) => setValue({...value, date_end: e + ''})} />}
+                {isOpen[1] && <DateCalendar value={value.date_end} setValue={(e) => setValue({ ...value, date_end: e + '' })} />}
             </div>
         </div>
         <div className={style.asideLine}></div>
         <div className={style.settingOption}>
             <div className={style.optionItems}>
-                <ToggleSetting text="Купе" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({...value, have_second_class: value}))} value={value.have_second_class} />
-                <ToggleSetting text="Плацкарт" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({...value, have_third_class : value}))} value={value.have_third_class } />
-                <ToggleSetting text="Сидячий" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({...value, have_fourth_class : value}))} value={value.have_fourth_class } />
-                <ToggleSetting text="Люкс" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({...value, have_first_class : value}))} value={value.have_first_class } />
-                <ToggleSetting text="Wi-Fi" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({...value, have_wifi : value}))} value={value.have_wifi } />
-                <ToggleSetting text="Экспресс" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({...value, have_express : value}))} value={value.have_express } />
+                <ToggleSetting text="Купе" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({ ...value, have_second_class: value }))} value={value.have_second_class} />
+                <ToggleSetting text="Плацкарт" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({ ...value, have_third_class: value }))} value={value.have_third_class} />
+                <ToggleSetting text="Сидячий" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({ ...value, have_fourth_class: value }))} value={value.have_fourth_class} />
+                <ToggleSetting text="Люкс" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({ ...value, have_first_class: value }))} value={value.have_first_class} />
+                <ToggleSetting text="Wi-Fi" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({ ...value, have_wifi: value }))} value={value.have_wifi} />
+                <ToggleSetting text="Экспресс" Icon={() => <Cupe />} setValue={(value) => dispatch(setState({ ...value, have_express: value }))} value={value.have_express} />
             </div>
         </div>
         <div className={style.asideLine}></div>
         <div className={style.settingPrice}>
             <h2 className={style.priceTitle}>Стоимость </h2>
-            <Rangeprice max={7000} min={1920} setValue={({min, max}) => dispatch(setState({...value, price_from : min, price_to : max}))}/>
+            <Rangeprice max={7000} min={1920} setValue={({ min, max }) => dispatch(setState({ ...value, price_from: min, price_to: max }))} />
         </div>
         <div className={style.asideLine}></div>
         <div className={style.settingSaid}>
-            <button className={`${style.settingBtn} ${style.settingBtn_forward}`}></button>
-            <p className={style.saidTitle}>Туда</p>
-            <button className={style.settingBtn_add}></button>
+            <div className={style.visiblePart}>
+                <button className={`${style.settingBtn} ${style.settingBtn_forward}`}></button>
+                <p className={style.saidTitle}>Туда</p>
+                <button className={style.settingBtn_add}  onClick={() => setOpenPart(!openPart)}></button>
+            </div>
+            <div className={style.hiddenPart}>
+                <p>Время отбытия</p>
+                <p>Время прибытия</p>
+            </div>
         </div>
         <div className={style.asideLine}></div>
-        <div className={style.settingSaid}>
-            <button className={`${style.settingBtn} ${style.settingBtn_reverce}`}></button>
-            <p className={style.saidTitle}>Обратно</p>
-            <button className={style.settingBtn_add}></button>
+        <div className={`${style.settingSaid} ${openPart && style.settingSaid_active}`}>
+        <div className={`${style.visiblePart} `}>
+                <button  className={`${style.settingBtn} ${style.settingBtn_reverce}` } ></button>
+                <p className={style.saidTitle}>Обратно</p>
+                <button className={style.settingBtn_add}  onClick={() => setOpenPart(!openPart)}></button>
+            </div>
+            <div className={style.hiddenPart}>
+                <p>Время отбытия</p>
+                <p>Время прибытия</p>
+            </div>
         </div>
     </section>
 }
